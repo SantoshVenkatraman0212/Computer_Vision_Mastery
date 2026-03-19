ed3c96d20033235f44d6c562d371537c4d396939
Santosh Venkatraman <vensan0212@gmail.com>
2026-03-19 17:26:39 +0000

    Implemented hysteresis on double threshold matrix to obtain well defined edges

---
+++ demos/hysteresis_demo.py
@@ -0,0 +1,49 @@
+'''
+This file demonstrates a full hysteresis demo with the image only having true edges of the
+objects
+'''
+# Importing necessary libraries
+import numpy as np
+from config.paths import sample_img_path, hysteresis_img_path
+from src.image_processing.gradients import (gaussian_blur, sobel_gradients, all_edges, 
+                                            orientation)
+from src.image_processing.edge_detection import radians_to_angle_conv, non_maximal_suppresion
+from src.image_processing.double_thresholding import double_threshold
+from src.image_processing.hysteresis import compute_hysteresis
+import cv2
+import os
+import matplotlib.pyplot as plt
+
+# Reading in the image
+sample_img =  f'{sample_img_path}/{os.listdir(sample_img_path)[0]}'
+sample_img = cv2.imread(sample_img, 0)
+# Smoothened image
+smooth_img = gaussian_blur(sample_img)
+# Sobel X and Y
+Gx = sobel_gradients(smooth_img)[0]
+Gy = sobel_gradients(smooth_img)[1]
+# Magnitude
+magnitude = all_edges(Gx, Gy)
+# Orientation
+theta = orientation(Gx, Gy)
+
+# Converting theta from radians to +ve angles
+theta_angle = radians_to_angle_conv(theta) 
+
+# Computing NMS
+nms = non_maximal_suppresion(magnitude, theta_angle)
+# Normalizing for consistency of pixel values
+norm_nms = (nms / nms.max()) * 255
+# Computing well-defined edge matrix using double thresholding
+double_threshold_matrix = double_threshold(nms)
+
+# Computing the hysteresis matrix
+hysteresis_matrix = compute_hysteresis(double_threshold_matrix)
+
+# Plotting the hysteresis image
+plt.figure(figsize = (10, 7))
+plt.imshow(hysteresis_matrix, cmap = 'gray', vmin = 0, vmax = 255)
+plt.title('Sample image after hysteresis')
+plt.axis('off')
+plt.tight_layout()
+plt.savefig(f'{hysteresis_img_path}/hysteresis_demo.png', dpi = 300, bbox_inches = 'tight')
--- src/image_processing/hysteresis.py
+++ src/image_processing/hysteresis.py
@@ -5,33 +5,52 @@ objects depicted in the image and effectively removing all noise
 # Importing necessary libraries
 import numpy as np
 
-# def compute_hysteresis(double_thresh: np.ndarray):
-#     '''
-#     This function performs hysteresis operation on the input double threshold image array.
-#     All strong edges and all weak edges connected to strong edges will be retained as they are
-#     the true edges of objects in the image. An isolated weak edge is noise.
+def compute_hysteresis(double_thresh: np.ndarray):
+    '''
+    This function performs hysteresis operation on the input double threshold image array.
+    All strong edges and all weak edges connected to strong edges will be retained as they are
+    the true edges of objects in the image. An isolated weak edge is noise.
 
-#     Args:
-#         double_thresh: input double threshold array
+    Args:
+        double_thresh: input double threshold array
 
-#     Returns:
-#         noiseless_img: image array with the true edges devoid of all noise
-#     '''
-#     H, W = double_thresh.shape
-#     noiseless_img = np.zeros(H, W, dtype = double_thresh.dtype)
+    Returns:
+        noiseless_img: image array with the true edges devoid of all noise
+    '''
+    H, W = double_thresh.shape
+    '''Making a copy of the double threshold matrix as we need updated pixel values at every 
+    stage'''
+    noiseless_img = double_thresh.copy()
 
-#     for i in range(H):
-#         for j in range(W):
-#             if double_thresh[i][j] == 75:
-#                 # Prev left
-#                 if (j - 1) >= 0:
-#                     if double_thresh[i - 1][j] == 255:
-#                         noiseless_img[i][j] = double_thresh[i][j]
-#                         continue
-#                 else:
-#                     if (i - 1) >= 0:
+    # Making a list of combinations of all 8 neighbors for each pixel
+    neighbors_list = [(0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0), (1, -1)]
+    # Getting the coordinates of only the strong pixels
+    strong_pix_list = []
+    for i in range(H):
+        for j in range(W):
+            if double_thresh[i][j] == 255:
+                strong_pix_list.append((i, j))
+    
+    while len(strong_pix_list) != 0:
+        '''As long as the stack isn't empty we check each neighbor, make them into strong pixels
+           and also add each neighbor to the stack. This way, every pixel is checked in a chain
+        '''
+        s_i, s_j = strong_pix_list.pop()
+        for n in neighbors_list:
+            # Getting neighbor coordinates
+            n_i = s_i + n[0]
+            n_j = s_j + n[1]
+            if n_i >= 0 and n_i < H and n_j >= 0 and n_j < W:
+                # Checking if the neighbor coordinates are valid (Reject Out of Bounds)
+                if noiseless_img[n_i][n_j] == 75:
+                    noiseless_img[n_i][n_j] = 255
+                    strong_pix_list.append((n_i, n_j))
+    
+    # Removing the residual noise
+    # only 0 and 255 should remain and isolated 75 should be 0
+    noiseless_img[noiseless_img == 75] = 0
+    
+    return noiseless_img
 
-#             else:
-#                 continue
 
     
